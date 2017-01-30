b464bb90e94515420d584a9460c52440e45c4d4e
Julia <jtalanova17@gmail.com>
2017-01-30 23:55:51 +0300

    1/2 of DLL

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,21 +1,54 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
+        if (this.length) {
+            node.prev = this._tail;
+            this._tail.next = node;
+        } else {
+            this._head = node;
+        }
+        this._tail = node;
+        this.length++;
+    }
 
-    head() {}
+    head() {
+        if (this._head) {
+            return this._head.data;
+        }
+        return null;
+    }
 
-    tail() {}
+    tail() {
+        if (this._tail) {
+            return this._tail.data;
+        }
+        return null;
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        if (this.length) {
+            return false;
+        }
+        return true;
+    }
 
-    clear() {}
+    clear() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
     deleteAt(index) {}
 
